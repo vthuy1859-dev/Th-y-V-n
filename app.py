d25b5b8dac9066356cd821d72dd97ecea8b92c62
Thúy Vân <vthuy1859@gmail.com>
2026-06-19 20:54:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề app
-st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 2_ NGÔ THỊ THÚY VÂN")
+st.title("APP TÍNH TIỀN GỬI TIẾT KIỆM_ĐỀ TÀI 5_ NGÔ THỊ THÚY VÂN")
 
 # Nhập dữ liệu
 C = st.number_input(
